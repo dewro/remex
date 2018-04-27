f1b2b706a3a0aa56a2d6498a0bce3c1a01f92406
hqxqyly <hqxfhlx@163.com>
2018-04-27 23:49:01 +0800

    ss

---
+++ src/remex-parent/remex-core/src/main/java/cn/dewro/remex/utils/CollectionUtils.java
@@ -0,0 +1,13 @@
+package cn.dewro.remex.utils;
+
+/**
+ * 集合工具类
+ * 
+ * @see org.apache.commons.collections.CollectionUtils
+ * 
+ * @author Qiaoxin.Hong
+ *
+ */
+public class CollectionUtils extends org.apache.commons.collections.CollectionUtils {
+
+}
---
+++ src/remex-parent/remex-core/src/main/java/cn/dewro/remex/utils/SortUtils.java
@@ -0,0 +1,14 @@
+package cn.dewro.remex.utils;
+
+/**
+ * 排序工具类
+ * 
+ * @author Qiaoxin.Hong
+ *
+ */
+public class SortUtils {
+	
+	public static void sort() {
+		
+	}
+}
